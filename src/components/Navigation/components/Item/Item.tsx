87f5cdac2bb1b40ba0bc0b79c7482410b2ce5b4e
Andrew Musgrave <andrew.musgrave@shopify.com>
2019-05-15 13:39:54 -0400

    Removed withContext from Navigation.Item

--- src/components/Navigation/components/Item/Item.tsx
+++ src/components/Navigation/components/Item/Item.tsx
@@ -3,15 +3,13 @@ import * as React from 'react';
 import {classNames} from '@shopify/react-utilities/styles';
 import {navigationBarCollapsed} from '../../../../utilities/breakpoints';
 
-import NavigationContext, {NavigationContextType} from '../../context';
-import {withAppProvider, WithAppProviderProps} from '../../../AppProvider';
+import NavigationContext from '../../context';
+import {WithAppProviderProps} from '../../../AppProvider';
 import Badge from '../../../Badge';
 import Icon, {Props as IconProps} from '../../../Icon';
 import Indicator from '../../../Indicator';
 import UnstyledLink from '../../../UnstyledLink';
-import withContext from '../../../WithContext';
-import compose from '../../../../utilities/react-compose';
-import {WithContextTypes} from '../../../../types';
+import {usePolaris} from '../../../../hooks';
 
 import styles from '../../Navigation.scss';
 
@@ -54,14 +52,6 @@ export interface Props extends ItemURLDetails {
   onClick?(): void;
 }
 
-export type CombinedProps = Props &
-  WithAppProviderProps &
-  WithContextTypes<NavigationContextType>;
-
-interface State {
-  expanded: boolean;
-}
-
 enum MatchState {
   MatchForced,
   MatchUrl,
@@ -70,224 +60,217 @@ enum MatchState {
   NoMatch,
 }
 
-export class BaseItem extends React.Component<CombinedProps, State> {
-  state: State = {
-    expanded: false,
-  };
+export default function Item({
+  url,
+  icon,
+  label,
+  subNavigationItems = [],
+  secondaryAction,
+  disabled,
+  onClick,
+  accessibilityLabel,
+  iconBody,
+  selected: selectedOverride,
+  badge,
+  new: isNew,
+  matches,
+  exactMatch,
+  matchPaths,
+  excludePaths,
+}: Props) {
+  const {intl} = usePolaris();
+  const {location, onNavigationDismiss} = React.useContext(NavigationContext);
+  const [expanded, setExpanded] = React.useState(false);
 
-  componentDidMount() {
-    navigationBarCollapsed().addListener(this.handleResize);
-  }
+  React.useEffect(() => {
+    navigationBarCollapsed().addListener(handleResize);
+    return () => {
+      navigationBarCollapsed().removeListener(handleResize);
+    };
+  }, []);
 
-  componentWillUnmount() {
-    navigationBarCollapsed().removeListener(this.handleResize);
-  }
+  const tabIndex = disabled ? -1 : 0;
 
-  render(): React.ReactElement<Props> {
-    const {expanded} = this.state;
+  const hasNewChild =
+    subNavigationItems.filter((subNavigationItem) => subNavigationItem.new)
+      .length > 0;
 
-    const {
-      url,
-      icon,
-      label,
-      subNavigationItems = [],
-      secondaryAction,
-      disabled,
-      onClick,
-      accessibilityLabel,
-      iconBody,
-      selected: selectedOverride,
-      badge,
-      new: isNew,
-      polaris: {intl},
-      context: {location, onNavigationDismiss},
-    } = this.props;
+  const indicatorMarkup = hasNewChild ? (
+    <span className={styles.Indicator}>
+      <Indicator pulse />
+    </span>
+  ) : null;
 
-    const tabIndex = disabled ? -1 : 0;
-
-    const hasNewChild =
-      subNavigationItems.filter((subNavigationItem) => subNavigationItem.new)
-        .length > 0;
-
-    const indicatorMarkup = hasNewChild ? (
-      <span className={styles.Indicator}>
-        <Indicator pulse />
-      </span>
-    ) : null;
-
-    const iconMarkup = iconBody ? (
+  const iconMarkup = iconBody ? (
+    <div className={styles.Icon}>
+      <Icon source={iconBody} />
+    </div>
+  ) : (
+    icon && (
       <div className={styles.Icon}>
-        <Icon source={iconBody} />
+        <Icon source={icon} />
       </div>
-    ) : (
-      icon && (
-        <div className={styles.Icon}>
-          <Icon source={icon} />
-        </div>
-      )
+    )
+  );
+
+  let badgeMarkup: React.ReactNode = null;
+  if (isNew) {
+    badgeMarkup = (
+      <Badge status="new" size="small">
+        {intl.translate('Polaris.Badge.STATUS_LABELS.new')}
+      </Badge>
+    );
+  } else if (typeof badge === 'string') {
+    badgeMarkup = (
+      <Badge status="new" size="small">
+        {badge}
+      </Badge>
+    );
+  } else {
+    badgeMarkup = badge;
+  }
+
+  const wrappedBadgeMarkup =
+    badgeMarkup == null ? null : (
+      <div className={styles.Badge}>{badgeMarkup}</div>
     );
 
-    let badgeMarkup: React.ReactNode = null;
-    if (isNew) {
-      badgeMarkup = (
-        <Badge status="new" size="small">
-          {intl.translate('Polaris.Badge.STATUS_LABELS.new')}
-        </Badge>
-      );
-    } else if (typeof badge === 'string') {
-      badgeMarkup = (
-        <Badge status="new" size="small">
-          {badge}
-        </Badge>
-      );
-    } else {
-      badgeMarkup = badge;
-    }
+  const itemContentMarkup = (
+    <React.Fragment>
+      {iconMarkup}
+      <span className={styles.Text}>
+        {label}
+        {indicatorMarkup}
+      </span>
+      {wrappedBadgeMarkup}
+    </React.Fragment>
+  );
 
-    const wrappedBadgeMarkup =
-      badgeMarkup == null ? null : (
-        <div className={styles.Badge}>{badgeMarkup}</div>
-      );
-
-    const itemContentMarkup = (
-      <React.Fragment>
-        {iconMarkup}
-        <span className={styles.Text}>
-          {label}
-          {indicatorMarkup}
-        </span>
-        {wrappedBadgeMarkup}
-      </React.Fragment>
-    );
-
-    if (url == null) {
-      const className = classNames(
-        styles.Item,
-        disabled && styles['Item-disabled'],
-      );
-
-      return (
-        <li className={styles.ListItem}>
-          <button
-            type="button"
-            className={className}
-            disabled={disabled}
-            aria-disabled={disabled}
-            aria-label={accessibilityLabel}
-            onClick={this.getClickHandler(onClick)}
-          >
-            {itemContentMarkup}
-          </button>
-        </li>
-      );
-    }
-
-    const secondaryActionMarkup = secondaryAction && (
-      <UnstyledLink
-        external
-        url={secondaryAction.url}
-        className={styles.SecondaryAction}
-        tabIndex={tabIndex}
-        aria-disabled={disabled}
-        aria-label={secondaryAction.accessibilityLabel}
-      >
-        <Icon source={secondaryAction.icon} />
-      </UnstyledLink>
-    );
-
-    const matchState = matchStateForItem(this.props, location);
-
-    const matchingSubNavigationItems = subNavigationItems.filter((item) => {
-      const subMatchState = matchStateForItem(item, location);
-      return (
-        subMatchState === MatchState.MatchForced ||
-        subMatchState === MatchState.MatchUrl ||
-        subMatchState === MatchState.MatchPaths
-      );
-    });
-
-    const childIsActive = matchingSubNavigationItems.length > 0;
-
-    const selected =
-      selectedOverride == null
-        ? matchState === MatchState.MatchForced ||
-          matchState === MatchState.MatchUrl ||
-          matchState === MatchState.MatchPaths
-        : selectedOverride;
-
-    const showExpanded = selected || expanded || childIsActive;
-
-    const itemClassName = classNames(
+  if (url == null) {
+    const className = classNames(
       styles.Item,
       disabled && styles['Item-disabled'],
-      selected && subNavigationItems.length === 0 && styles['Item-selected'],
-      showExpanded && styles.subNavigationActive,
-    );
-
-    let secondaryNavigationMarkup: React.ReactNode = null;
-
-    if (subNavigationItems.length > 0 && showExpanded) {
-      const longestMatch = matchingSubNavigationItems.sort(
-        ({url: firstUrl}, {url: secondUrl}) =>
-          secondUrl.length - firstUrl.length,
-      )[0];
-
-      secondaryNavigationMarkup = (
-        <div className={styles.SecondaryNavigation}>
-          <Secondary expanded={showExpanded}>
-            {subNavigationItems.map((item) => {
-              const {label, ...rest} = item;
-              return (
-                <Item
-                  {...rest}
-                  key={label}
-                  label={label}
-                  matches={item === longestMatch}
-                  onClick={onNavigationDismiss}
-                />
-              );
-            })}
-          </Secondary>
-        </div>
-      );
-    }
-
-    const className = classNames(
-      styles.ListItem,
-      secondaryAction && styles['ListItem-hasAction'],
     );
 
     return (
-      <li className={className}>
-        <UnstyledLink
-          url={url}
-          className={itemClassName}
-          tabIndex={tabIndex}
+      <li className={styles.ListItem}>
+        <button
+          type="button"
+          className={className}
+          disabled={disabled}
           aria-disabled={disabled}
           aria-label={accessibilityLabel}
-          onClick={this.getClickHandler(onClick)}
+          onClick={getClickHandler(onClick)}
         >
           {itemContentMarkup}
-        </UnstyledLink>
-        {secondaryActionMarkup}
-        {secondaryNavigationMarkup}
+        </button>
       </li>
     );
   }
 
-  private handleResize = () => {
-    if (!navigationBarCollapsed().matches && this.state.expanded) {
-      this.setState({expanded: false});
-    }
-  };
+  const secondaryActionMarkup = secondaryAction && (
+    <UnstyledLink
+      external
+      url={secondaryAction.url}
+      className={styles.SecondaryAction}
+      tabIndex={tabIndex}
+      aria-disabled={disabled}
+      aria-label={secondaryAction.accessibilityLabel}
+    >
+      <Icon source={secondaryAction.icon} />
+    </UnstyledLink>
+  );
 
-  private getClickHandler = (onClick: Props['onClick']) => {
+  const matchState = matchStateForItem(
+    {url, matches, exactMatch, matchPaths, excludePaths},
+    location,
+  );
+
+  const matchingSubNavigationItems = subNavigationItems.filter((item) => {
+    const subMatchState = matchStateForItem(item, location);
+    return (
+      subMatchState === MatchState.MatchForced ||
+      subMatchState === MatchState.MatchUrl ||
+      subMatchState === MatchState.MatchPaths
+    );
+  });
+
+  const childIsActive = matchingSubNavigationItems.length > 0;
+
+  const selected =
+    selectedOverride == null
+      ? matchState === MatchState.MatchForced ||
+        matchState === MatchState.MatchUrl ||
+        matchState === MatchState.MatchPaths
+      : selectedOverride;
+
+  const showExpanded = selected || expanded || childIsActive;
+
+  const itemClassName = classNames(
+    styles.Item,
+    disabled && styles['Item-disabled'],
+    selected && subNavigationItems.length === 0 && styles['Item-selected'],
+    showExpanded && styles.subNavigationActive,
+  );
+
+  let secondaryNavigationMarkup: React.ReactNode = null;
+
+  if (subNavigationItems.length > 0 && showExpanded) {
+    const longestMatch = matchingSubNavigationItems.sort(
+      ({url: firstUrl}, {url: secondUrl}) => secondUrl.length - firstUrl.length,
+    )[0];
+
+    secondaryNavigationMarkup = (
+      <div className={styles.SecondaryNavigation}>
+        <Secondary expanded={showExpanded}>
+          {subNavigationItems.map((item) => {
+            const {label, ...rest} = item;
+            return (
+              <Item
+                {...rest}
+                key={label}
+                label={label}
+                matches={item === longestMatch}
+                onClick={onNavigationDismiss}
+              />
+            );
+          })}
+        </Secondary>
+      </div>
+    );
+  }
+
+  const className = classNames(
+    styles.ListItem,
+    secondaryAction && styles['ListItem-hasAction'],
+  );
+
+  return (
+    <li className={className}>
+      <UnstyledLink
+        url={url}
+        className={itemClassName}
+        tabIndex={tabIndex}
+        aria-disabled={disabled}
+        aria-label={accessibilityLabel}
+        onClick={getClickHandler(onClick)}
+      >
+        {itemContentMarkup}
+      </UnstyledLink>
+      {secondaryActionMarkup}
+      {secondaryNavigationMarkup}
+    </li>
+  );
+
+  function handleResize() {
+    if (!navigationBarCollapsed().matches && expanded) {
+      setExpanded(false);
+    }
+  }
+
+  function getClickHandler(onClick: Props['onClick']) {
     return (event: React.MouseEvent<HTMLElement>) => {
       const {currentTarget} = event;
-      const {
-        subNavigationItems,
-        context: {location, onNavigationDismiss},
-      } = this.props;
 
       if (currentTarget.getAttribute('href') === location) {
         event.preventDefault();
@@ -299,7 +282,7 @@ export class BaseItem extends React.Component<CombinedProps, State> {
         navigationBarCollapsed().matches
       ) {
         event.preventDefault();
-        this.setState(({expanded}) => ({expanded: !expanded}));
+        setExpanded(!expanded);
       } else if (onNavigationDismiss) {
         onNavigationDismiss();
         if (onClick && onClick !== onNavigationDismiss) {
@@ -312,7 +295,7 @@ export class BaseItem extends React.Component<CombinedProps, State> {
         onClick();
       }
     };
-  };
+  }
 }
 
 export function isNavigationItemActive(
@@ -385,12 +368,3 @@ function matchStateForItem(
     : safeStartsWith(location, url);
   return matchesUrl ? MatchState.MatchUrl : MatchState.NoMatch;
 }
-
-export const Item = compose<Props>(
-  withAppProvider<Props>(),
-  withContext<Props, WithAppProviderProps, NavigationContextType>(
-    NavigationContext.Consumer,
-  ),
-)(BaseItem);
-
-export default Item;
